0a48d79cdbd5574ef4b97b7f5d7c3bc8a8ae45dc
Kuharenko <iamksergey@mail.ru>
2020-09-08 10:40:56 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
-import React, {useEffect, useMemo, useState} from 'react';
-
-function complexCompute(num) {
-    console.log('complexCompute')
-    let i = 0
-    while (i < 1000000000) i++
-    return num * 2
-}
+import React, {useEffect, useState} from 'react';
 
 function App() {
 
-    const [number, setNumber] = useState(42)
-    const [colored, setColored] = useState(false)
-
+    const [type, setType] = useState('users')
+    const [data, setData] = useState([])
+    const [pos, setPos] = useState({
+        x: 0,
+        y: 0
+    })
 
     // #1
-
-    // const styles = useMemo(() => {
-    //         return {
-    //             color: colored ? 'darkred' : 'black'
-    //         }
-    //     }
-    // )
-
-    // с помощью useMemo мы кешируем нашу ф-ию с number,
-    // тем самым более не вызываем ф-ию при изменении
-    // св-ва colored
-    const computed = useMemo(() => {
-        return complexCompute(number)
-    }, [number])
+    // Вызывается каждый раз при перерендеринге
+    // useEffect(() => {})
 
     // #2
-    const styles = useMemo(() => ({
-        color: colored ? 'darkred' : 'black'
-    }), [colored])
+    // Указываем зависимость
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${type}`)
+            .then(response => response.json())
+            .then(json => setData(json))
+
+        // удаляем слушателя (делаем отписки)
+        return () => {
+            console.log('clean type')
+        }
+    }, [type])
+
+    // #3
+    // Вызывается один раз
+    useEffect(() => {
+        console.log('ComponentDidMount')
+    }, [])
+
+    // #4
+    // Добавляем слушателя
+    const mouseMoveHandler = event => {
+        setPos({
+            x: event.clientX,
+            y: event.clientY,
+        })
+    }
 
     useEffect(() => {
-        console.log('Styles changed')
-    }, [styles])
+        window.addEventListener('mousemove', mouseMoveHandler)
 
-    // !useMemo вызывать в тех случаях, когда это может ударить по производительности
+        // удаляем слушателя
+        return () => {
+            window.removeEventListener('mousemove', mouseMoveHandler)
+        }
+    }, [])
 
     return (
         <div className="App">
-            <h1 style={styles}>Вычисляемое свойство: {computed}</h1>
-            <button onClick={() => setNumber(prev => prev + 1)} className={'btn btn-success'}>Добавить</button>
-            <button onClick={() => setNumber(prev => prev - 1)} className={'btn btn-danger'}>Убрать</button>
-            <button onClick={() => setColored(prev => !prev)} className={'btn btn-warning'}>Изменить</button>
+            <h1>Ресурс: {type}</h1>
+
+            <button onClick={() => setType('users')}>Пользователи</button>
+            <button onClick={() => setType('todos')}>Todos</button>
+            <button onClick={() => setType('posts')}>Посты</button>
+
+            <pre>
+                {JSON.stringify(pos, null, 2)}
+            </pre>
         </div>
     );
 }
